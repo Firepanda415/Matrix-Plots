d0e83e84a31e5c3707e55b896db7c8f0b7f0b2b0
Muqing Zheng <dzhengmq@gmail.com>
2021-03-15 23:39:19 -0400

    Plot transition matrix

---
+++ obj.m
@@ -0,0 +1,29 @@
+function y=obj(x)
+% function y=obj(x)
+% First n variables of x are Pr(0|0), 
+% last n variables of x are Pr(1|1)
+    n = length(x)/2;
+    p0m0 = x(1:n);
+    p1m1 = x(n+1:2*n);
+    As = zeros(2,2,n);
+    % Construct matrix under ind assumption
+    for i = 1:n
+        As(:,:,i) = [[p0m0(i) 1-p1m1(i)];...
+            [1-p0m0(i) p1m1(i)]];
+    end
+    
+    A_ind = As(:,:,1);
+    for j = 2:n
+        A_ind = kron(A_ind, As(:,:,j));
+    end
+
+%     res = [[1-p0m0(1) p1m1(1)];[p0m0(1) 1-p1m1(1)]];
+%     for j = 2:n
+%         res = kron(res, [[1-p0m0(j) p1m1(j)];[p0m0(j) 1-p1m1(j)]]);
+%     end
+    
+
+    A_full = readmatrix('BelemData/Afull.txt');
+       
+    y = norm(A_full - A_ind,'fro')^2;
+end
---
+++ solution_plots.m
@@ -0,0 +1,30 @@
+clc;clear;close all;
+% Author: Muqing Zheng
+
+% p0m0 = sym("e", [2 1], 'real'); % epsilon
+% p1m1 = sym("x", [2 1], 'real');
+
+% r = 1.05; % p0m0/p1m1
+% x = 0.8:0.001:1; %p0m0 for qubit 0
+% y = 0.8:0.001:1; %p0m0 for qubit 1
+% [X,Y] = meshgrid(x,y);
+% [size1, size2] = size(X);
+% Z = zeros(size1, size2); 
+% for i = 1:size1
+%     for j = 1:size2
+%         Z(i,j) = obj([X(i,j), Y(i,j), r*X(i,j),  r*Y(i,j)]); 
+%     end
+% end
+% f = figure(1);
+% mesh(X,Y,Z);
+% xlabel('Pr(0|0) for qubit 0');
+% ylabel('Pr(0|0) for qubit 1');
+% title('||A_{full}-A_{ind}||_F^2');
+%% Optimization
+% First n variables of x are Pr(0|0), 
+% last n variables of x are Pr(1|1)
+
+num_qubits = 5;
+x0 = ones(1,2*num_qubits)*0.9;
+[xopt,fval,exitflag,output] = fminsearch(@obj, x0);
+writematrix(xopt, 'BelemData/Aopt.txt')
